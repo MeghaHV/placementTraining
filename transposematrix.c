85b0edd6a571f4ad37bb89f0317414606647ad2f
Megha H V <78419472+MeghaHV@users.noreply.github.com>
2023-04-25 09:55:31 +0530

    Create transposematrix.c

---
+++ transposematrix.c
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n,m;
+  int a[50][50],b[50][50];
+  scanf("%d",&n);
+  scanf("%d",&m);
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      scanf("%d",&a[i][j]);
+    }
+  }
+  for(int i=0;i<m;i++){
+    for(int j=0;j<n;j++){
+      b[i][j]=a[j][i];
+      printf("%d ",b[i][j]);
+    }
+  }
+  
+}
